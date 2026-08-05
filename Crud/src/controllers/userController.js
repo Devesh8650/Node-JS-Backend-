5185ad7ba493c03887deb64d897c333538b2570b
Devesh8650 <deveshkumarbsrupp@gmail.com>
2026-08-05 13:10:01 +0530

    create the getById api for user and show the user id base

--- Crud/src/controllers/userController.js
+++ Crud/src/controllers/userController.js
@@ -27,9 +27,33 @@ const getUsers = async (req, res) => {
   } catch (err) {
     res.status(500).json({
       success: false,
-      message: error.message,
+      message: err.message,
     });
   }
 };
 
-module.exports = { createUser,getUsers };
+// GET USER BY ID
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+module.exports = { createUser, getUsers, getUserById };
