99433e100de2b8cd59be00b1d09f8023a832e0fc
Devesh8650 <deveshkumarbsrupp@gmail.com>
2026-08-06 15:01:03 +0530

    Created production-ready schema with validation rules

--- Crud/src/models/userModel.js
+++ Crud/src/models/userModel.js
@@ -3,14 +3,33 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    // required: true,
+    // String Validation Production Level Schema creatr using Validations
+    required: [true, "Name is required"],
+    trim: true,
+    minLength: [3, "Name must be at least 3 characters"],
+    maxLength: [20, "Name cannot exceed 20 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    trim: true,
+    lowercase: true,
+    unique: true,
   },
   role: {
     type: String,
-    required: true,
+    required: [true, "Role is required"],
+    trim: true,
+    enum: {
+      values: ["Admin", "HR", "Student", "Instructor"],
+      message: "{VALUE} is not a valid role",
+    },
   },
   age: {
     type: Number,
+    min: [18, "Age must be at least 18 years"],
+    max: [60, "Age cannot be greater than 60 years"],
   },
   // SOFT DELETE USER KE LIYE
   isDeleted: {
